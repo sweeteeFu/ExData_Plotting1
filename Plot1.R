07e520fc1f5ab4476a932ac89abfee9402dc57d6
sweeteeFu <sfu@swinburne.edu.my>
2016-09-01 12:28:33 +0800

    Added Plot1.R and Plot1.png

---
+++ Plot1.R
@@ -0,0 +1,23 @@
+CreatePlot1 <- function(){
+  ## Check path existence
+  if(!file.exists("./R scripts")){
+    dir.create("./R scripts")
+  }
+  
+  ## Unzip the files
+  unzip(zipfile = "./R scripts/household_power_consumption.zip",exdir = "./R scripts")
+  
+  ## Loading the data set from 1/2/2007 - 2/2/2007
+  household_power_consumption <- read.table("./R scripts/household_power_consumption.txt", header=TRUE, sep=";", skip=66637, nrows=2880, col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),na.strings="?")
+  
+  ## Create a new variable called Timestamp which will combine the both the Date and Time variables
+  household_power_consumption$Timestamp<-strptime(paste(household_power_consumption$Date,household_power_consumption$Time),"%d/%m/%Y %H:%M:%S")
+  
+  ## plot the histogram
+  hist(household_power_consumption$Global_active_power,col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+  
+  ## saving the plot in png file
+  dev.copy(png,"Plot1.png",width=480,height=480)
+  dev.off()
+  
+}
